c1c1ea2a42d3d4bcf746a935f4e60e30bd73fd21
lingtran <lxiutran@gmail.com>
2017-06-14 23:25:28 -0500

    Add setup for callbacks via tutorial

--- index.js
+++ index.js
@@ -23,3 +23,61 @@ app.get("/webhook", function (req, res) {
         res.sendStatus(403);
     }
 })
+
+// All callbacks for Messenger
+app.post("/webhook", function (req, res) {
+  if (req.body.object == "page") {
+    req.body.entry.forEach(function(entry) {
+      entry.messaging.forEach(function(event) {
+        if (event.postback) {
+          processPostback(event);
+        }
+      });
+    });
+    res.sendStatus(200);
+  }
+})
+
+function processPostback(event) {
+  var senderId = event.sender.id;
+  var payload = event.postback.payload;
+
+  if (payload === "Greeting") {
+    request({
+      url: "https://graph.facebook.com/v2.6" + senderId,
+      qs: {
+        access_token: process.env.PAGE_ACCESS_TOKEN,
+        fields: "first_name"
+      },
+      method: "GET"
+    }, function(error, response, body) {
+      var greeting = "";
+      if (error) {
+        console.log("Error getting user's name: " + error);
+      } else {
+        var bodyObj = JSON.parse(bdoy);
+        name = bodyObj.first_name;
+        greeting = "Hi " + name + ". ";
+      }
+      var message = greeting + "My name is MeowBot. I hold knowledge of feline matters. What do you seek to know about my peers?";
+      sendMessage(senderId, {text: message});
+    });
+  }
+}
+
+// sends message to user
+function sendMessage(recipientId, message) {
+  request({
+    url: "https://graph.facebook.com/v2.6/me/messages",
+    qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+    method: "POST",
+    json: {
+      recipient: {id: recipientId},
+      message: message,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log("Error sending message: " + response.error);
+    }
+  });
+}
